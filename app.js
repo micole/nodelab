1e55f0fee19e06833f070607e9bada9190996285
Paul Shahid <pshahid90@gmail.com>
2014-02-19 14:05:20 -0500

    Added an IRC bot, created a way to login and post things with the bot

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 var http = require('http'),
+    https = require('https'),
     express = require('express'),
     passport = require('passport'),
     channel = 'twitchplayspokemon',
     util = require('util'),
-    TwitchtvStrategy = require('passport-twitchtv').Strategy;
+    fs = require('fs'),
+    TwitchtvStrategy = require('passport-twitchtv').Strategy,
+    key = fs.readFileSync(__dirname + '/dildos.key'),
+    cert = fs.readFileSync(__dirname + '/certificate.pem'),
+    irc = require('irc'),
+    twitchAccessToken = null,
+    bot = null;
+    // nodebot = require('./nodebot');
 
 //Constants/Finals
 //My Twitch app url is 32062
@@ -26,6 +34,7 @@ passport.use(new TwitchtvStrategy({
   },
   function(accessToken, refreshToken, profile, done) {
     console.log(profile);
+    twitchAccessToken = accessToken;
     // asynchronous verification, for effect...
     process.nextTick(function () {
       
@@ -77,7 +86,7 @@ app.get('/login', function(req, res){
 //   redirecting the user to Twitch.tv.  After authorization, Twitch.tv will
 //   redirect the user back to this application at /auth/twitchtv/callback
 app.get('/auth/twitchtv',
-    passport.authenticate('twitchtv', { scope: [ 'user_read' ] }),
+    passport.authenticate('twitchtv', { scope: [ 'user_read', 'chat_login', 'channel_read' ] }),
     function(req, res){
     // The request will be redirected to Twitch.tv for authentication, so this
     // function will not be called.
@@ -105,13 +114,78 @@ app.get('/logout', function(req, res){
 //////////////////////////////////////////////////////////////////
 
 app.get('/test', function(req, res) {
-    // res.redirect('/');
-    console.log("Test being called");
-    req.end();
+    connectIrcBot();
+
+    res.render('index', {user: req.user});
 });
+
+app.get('/chat/sayup', function(req, res) {
+    if (bot) {
+        bot.say("#twitchplayspokemon", "UUUUUUUUUUUUUP");
+    }
+
+    res.render('index', {user: req.user});
+})
 
 app.listen(3000);
 
+function connectIrcBot() {
+    console.log(twitchAccessToken);
+    var channel = "#twitchplayspokemon";
+
+    var config = {
+        channels: [channel],
+        server: "199.9.252.26",
+        botName: "diaper_sniper",
+        port: 6667,
+        showErrors: true
+    };
+
+    var irc = require('irc');
+
+    bot = new irc.Client(
+        config.server,
+        config.botName,
+        {
+            channels: config.channels,
+            password: "oauth:" + twitchAccessToken,
+            port: 6667
+        }
+    );
+
+    bot.addListener("message", function(from, to, text, message) {
+        // bot.say(config.channels[0],)
+        // console.log(from);
+        if (from === "dipperderp") {
+            console.log(message);
+            console.log("Diaper sniper said something!");
+            console.log(text);
+        }
+
+    });
+
+    bot.addListener("registered", function(message) {
+        console.log("IRC bot connected to the server");
+        // bot.send(".join #twitchplayspokemon");
+        // bot.join("#diaper_sniper", function(arg) {
+        //     console.log("Joined twitch plays pokemon");
+        // });
+    });
+
+    bot.addListener('kill', function(nick, reason, channels, message) {
+        console.log("Connection died with IRC");
+        console.log(reason);
+    });
+
+    bot.addListener('join' + channel, function(nick, message) {
+        // console.log(nick);
+        // console.log("User joined: " + nick);
+        if (nick === "diaper_sniper") {
+            console.log("We've connected to the channel");
+            // bot.say(channel, 'UUUUUUUUUUUUUP');
+        }
+    });
+}
 
 // Simple route middleware to ensure user is authenticated.
 //   Use this route middleware on any resource that needs to be protected.  If
